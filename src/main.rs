919e40f87971a4410d0d0f39b3be6879dc1f176f
root <root@xiaoxx.utek.com>
2024-01-22 13:17:27 +0800

    sqlx query test ok

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,37 @@
-use sqlx::mysql::MySqlPool;
-
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // 建立 MySQL 连接池
-    let pool = MySqlPool::builder()
-        .max_size(5)
-        .build(&"mysql://user:password@localhost/database")
-        .await?;
-
-    // 在此处执行增删改查操作
-    get_users(pool)
-    Ok(())
-}
-use sqlx::mysql::MySqlQueryAs;
-
-#[derive(Debug, sqlx::FromRow)]
+use sqlx::mysql::{ MySqlPoolOptions};
+use time::OffsetDateTime;
+#[derive(Debug,  sqlx::FromRow)]
 struct User {
     id: i32,
     username : String,
-    password: String,
-    enable: i8,
-    createTime: String,
-    updateTime: String,
+    createTime:OffsetDateTime,
 }
 
-async fn get_users(pool: &MySqlPool) -> Result<Vec<User>, sqlx::Error> {
-    let users = sqlx::query_as::<_, User>("SELECT * FROM user")
-        .fetch_all(pool)
+#[tokio::main]
+async fn main() -> Result<(), sqlx::Error> {
+    let pool = MySqlPoolOptions::new().connect("mysql://naive_admin:naive_admin_pass@localhost:33069/naive_admin").await?;
+
+
+  let rows = sqlx::query_as::<_, User>("SELECT * FROM user")
+        .fetch_all(&pool)
         .await?;
 
-    Ok(users)
+    println!("{:#?}", rows);
+
+
+
+
+//
+//      test ok
+//     let pool = MySqlPoolOptions::new().connect("mysql://naive_admin:naive_admin_pass@localhost:33069/naive_admin").await?;
+//
+//
+//      let rows = sqlx::query!("select * from user") .fetch_all(&pool).await?;
+//      println!("{:#?}", rows);
+
+    Ok(())
 }
+
+
+
+
