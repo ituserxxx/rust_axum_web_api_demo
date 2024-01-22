5bd89695209f8a16772b363f2b3917183630d4bc
root <root@xiaoxx.utek.com>
2024-01-22 15:11:12 +0800

    sqlx insert test ok

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,48 @@
-use sqlx::mysql::{ MySqlPoolOptions};
+use sqlx::mysql::{ MySqlPoolOptions,MySqlPool};
 use time::OffsetDateTime;
+mod tools;
 #[derive(Debug,  sqlx::FromRow)]
 struct User {
-    id: i32,
     username : String,
-    createTime:OffsetDateTime,
+    password : String,
+    enable : i8,
+    createTime : OffsetDateTime,
+    updateTime : OffsetDateTime,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), sqlx::Error> {
     let pool = MySqlPoolOptions::new().connect("mysql://naive_admin:naive_admin_pass@localhost:33069/naive_admin").await?;
 
+//     update
 
-  let rows = sqlx::query_as::<_, User>("SELECT * FROM user")
-        .fetch_all(&pool)
+//     insert ok
+    let user = User {
+        username: "John2".to_string(),
+        password: tools::md5_crypto("123456".to_string()),
+        enable:1,
+        createTime: OffsetDateTime::now_utc(),
+        updateTime: OffsetDateTime::now_utc(),
+    };
+    let query = "INSERT INTO user (username, password, enable, createTime, updateTime) VALUES (?, ?, ?, ?, ?)";
+
+    let result = sqlx::query(&query)
+        .bind(&user.username)
+        .bind(&user.password)
+        .bind(&user.enable)
+        .bind(&user.createTime)
+        .bind(&user.updateTime)
+        .execute(&pool)
         .await?;
 
-    println!("{:#?}", rows);
+    println!("{:?}", result);
 
 
-
-
-//
-//      test ok
-//     let pool = MySqlPoolOptions::new().connect("mysql://naive_admin:naive_admin_pass@localhost:33069/naive_admin").await?;
-//
-//
-//      let rows = sqlx::query!("select * from user") .fetch_all(&pool).await?;
-//      println!("{:#?}", rows);
+//     query ok
+//     let rows = sqlx::query_as::<_, User>("SELECT * FROM user").fetch_all(&pool).await?;
+//     println!("{:#?}", rows);
 
     Ok(())
 }
-
-
 
 
---
+++ src/tools/mod.rs
@@ -0,0 +1,11 @@
+
+use md5::Md5;
+use md5::Digest;
+
+pub fn md5_crypto(password:String) ->String{
+    let hashed_password = format!("{:x}", Md5::digest(password.as_bytes()));
+    hashed_password
+}
+pub fn are_strings_equal(input_password: &str, password: &str) -> bool {
+    password ==  format!("{:x}", Md5::digest(input_password.as_bytes()))
+}
