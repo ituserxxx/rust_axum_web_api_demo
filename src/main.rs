c83eede9261da0f460ac268177a7181b73c65692
root <root@xiaoxx.utek.com>
2024-02-27 17:54:32 +0800

    test middle params to handle is ok

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,76 @@
+
 use axum::{
-    Router,http,
+     Router, http,
     http::StatusCode,
     routing::get,
     response::{IntoResponse, Response},
     middleware::{self, Next},
     extract::{Request, Extension},
+    extract::rejection::JsonRejection,
 };
 
+use serde::{Deserialize, Serialize};
+use axum_extra::extract::WithRejection;
+use thiserror::Error;
+use axum::Json;
 
-#[derive(Clone)]
+#[derive(Clone,Debug)]
 pub struct CurrentUser {
-    id:i64
- }
+    pub id: i64,
+}
 
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct JwtDnReq {
+    #[serde(default)]
+    pub name: Option<String>,
+}
 async fn auth(mut req: Request, next: Next) -> Result<Response, StatusCode> {
     let auth_header = req.headers()
         .get(http::header::AUTHORIZATION)
         .and_then(|header| header.to_str().ok());
-   let auth_header = if let Some(auth_header) = auth_header {
-           println!("666");
-           auth_header
-       } else {
-           println!("555");
-           return Err(StatusCode::UNAUTHORIZED);
-       };
 
-       if let Some(current_user) = authorize_current_user(&auth_header).await {
-           // insert the current user into a request extension so the handler can
-           // extract it
-           req.extensions_mut().insert(current_user);
+    let auth_header = if let Some(auth_header) = auth_header {
+        auth_header
+    } else {
+        return Err(StatusCode::UNAUTHORIZED);
+    };
 
-           println!("444");
-           Ok(next.run(req).await)
-       } else {
-           println!("333");
-           Err(StatusCode::UNAUTHORIZED)
-       }
+    if let Some(current_user) = authorize_current_user(auth_header).await {
+        // insert the current user into a request extension so the handler can
+        // extract it
+        req.extensions_mut().insert(current_user);
+
+        Ok(next.run(req).await)
+    } else {
+        Err(StatusCode::UNAUTHORIZED)
+    }
 }
 
 async fn authorize_current_user(auth_token: &str) -> Option<CurrentUser> {
-     println!("222");
-     println!("{}",auth_token);
-     return Some(CurrentUser{id:1});
+    return Some(CurrentUser{id:1});
 }
-async fn handler() -> impl IntoResponse {
-   println!("1111");
-    "JWT Encode"
+
+async fn handler(
+    // extract the current user, set by the middleware
+    Extension(current_user): Extension<CurrentUser>,
+    req: Request,
+) {
+      println!("req: {:?}", req);
+      println!("Current user: {:?}", current_user);
 }
+
+
+
+
 #[tokio::main]
 async fn main() {
-// curl -X POST -H "Content-Type: application/json" http://127.0.0.1:8061/hello/jwt_dn
+// curl -X GET -H "Content-Type: application/json" -H "Authorization: xxxxxxxxx" -d '{"name":"xx1"}' http://127.0.0.1:8061/
+// curl -X GET -H "Content-Type: application/json"  -d '{"name":"xx1"}' http://127.0.0.1:8061/
 
-    let handler_r = Router::new()
-        .route("/a", get(handler))
-        .route_layer(middleware::from_fn(auth));
+
     let app = Router::new()
-            .route("/", get(|| async { "☺ welcome to Rust" }))
-            .nest("/h",handler_r);
+        .route("/", get(handler))
+        .layer(middleware::from_fn(auth));
     let routerInit = app.into_make_service();
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8061").await.unwrap();
@@ -65,3 +79,5 @@ async fn main() {
     axum::serve(listener, routerInit).await.unwrap();
 
 }
+
+
